7769bea5a767c51cef731fcd43e7822a76c52000
Buezrello <igindin@gmail.com>
2019-01-05 15:54:47 +0200

    Create testbatch.bat

---
+++ testbatch.bat
@@ -0,0 +1,12 @@
+@echo off
+echo "this script is from github"
+set NUMBERONE=%one%
+set NUMBERTWO=%two%
+set /A NUMBER=%NUMBERONE% + %NUMBERTWO%
+echo %NUMBERONE%
+echo %NUMBERTWO%
+echo %NUMBER%
+echo "finished"
+echo "AAAAAAAAAAAAAA"
+echo "BBBBBBBBBBBBBBB"
+echo "ccccCCC"
